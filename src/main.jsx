bbad536901c81f275abc411aa0f97563a88bb5e2
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-05-01 11:29:44 -0400

    testing responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import { createContext } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+       
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
