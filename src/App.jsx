23594464d16075a6912b1dfcb36f95208bca3592
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-07-12 14:58:32 -0400

    made app way better

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,46 @@ import { TaskContext } from './main'
 import { v4 as uuidv4 } from 'uuid' //used to prevent index-related bugs
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import confetti from 'canvas-confetti';
 
 function App() {
   const [taskName, setTaskName] = useState('') //first item in task array, used in creation of task
   const { state, dispatch } = useContext(TaskContext)
-  const [editable, setEditable] = useState(false) //used to allow editing of task names/descriptions
+  const [editStates, setEditStates] = useState({}) //used to allow editing of task names/descriptions
   const [dueDate, setDueDate] = useState(new Date()) //used to set task due dates on creation
   const currentTime = new Date() //used to check if a task is overdue
-  
+
+
+
+  const handleEditClick = (task) => {
+    setEditStates((prevEditStates) => {
+      const newEditStates = { ...prevEditStates, [task.id]: !prevEditStates[task.id] };
+
+      if (newEditStates[task.id]) {
+        // Task is now editable, set emoji to '✔️'
+        task.editEmoji = '✔️';
+      } else {
+        // Task is no longer editable, dispatch the edit action and set emoji to '✏️'
+        dispatch({ type: 'editTask', name: task.name, id: task.id, status: task.status, due: task.due });
+        task.editEmoji = '✏️';
+      }
+
+      return newEditStates;
+    });
+  };
+
+  const handleTaskNameChange = (task, newName) => {
+    // Update the task name locally when edited
+    task.name = newName;
+  };
+
   useEffect(() => { //saves current state to local storage every time state is changed
     localStorage.setItem('state', JSON.stringify(state))
   }, [state])
-  
-  const display = state.tasks?.map(task => { //displays task after it is created
+
+  const sortedTasks = [...state.tasks].sort((a, b) => a.due - b.due)
+
+  const display = sortedTasks?.map(task => { //displays task after it is created
     console.log(task)
     let textColor = 'black' //default text color
     if (task.status === 'complete') {//if: green, else: red format prevents completed tasks from unnecessarily being marked overdue
@@ -28,74 +55,66 @@ function App() {
     let newDate = new Date(task.due) //turns due date integer back into an object for display
     console.log(newDate)
     return (
-      <div key = { task.id }>
-      <input defaultValue={ task.name } readOnly = {!editable} style = {{ color: textColor }} //task can only be edited while editable is true
-      onChange={(e) => {
-        if (editable === true) {
-        setTaskName(e.target.value) //allows task name to be changed in local storage when edit button is pressed again
-        console.log(task)
-        }
-      }
-    }
-      >
-      </input>
-        Due: { newDate.toString() }
-      <button className = 'editTask' onClick = {() => {
-        setEditable(!editable) //allows for editing of task, or saves edit and blocks further editing unless button is pressed again
-        if (editable == true) { //if editable is false, task could not have been changed to there is no need to dispatch edit
-        dispatch({ type:'editTask', name: taskName, id: task.id, status: task.status, due: task.due}) //saves new task name to local storage
-        }
-        setTaskName('') //for some reason the edited task name also appears in the add field, this clears it when edit is dispatched
-      }
-      }>
-      Edit
-      </button>
-      <button className = 'deleteTask' onClick = {() => {
-        dispatch({ type:'deleteTask',name: taskName, id: task.id, status: task.status, due: task.due})
-      }  //deletes task corresponding to the delete button that was clicked
+      <div className='task' key={task.id}>
+        <input
+          defaultValue={task.name}
+          readOnly={!editStates[task.id]}
+          style={{ color: textColor }} //task can only be edited while editable is true
+          onChange={(e) => handleTaskNameChange(task, e.target.value)}
+        />
+        &nbsp;
+        Due: {newDate.toDateString()}
+        &nbsp;
+        <button className='editTask' onClick={() => handleEditClick(task)}>
+          {task.editEmoji || '✏️'}
+        </button>
+        <button className='deleteTask' onClick={() => {
+          dispatch({ type: 'deleteTask', name: taskName, id: task.id, status: task.status, due: task.due })
+        }  //deletes task corresponding to the delete button that was clicked
         }>
-      Delete
-      </button>
-      <button className = 'markComplete' onClick = {() => {
-        dispatch({ type:'markComplete',name: taskName, id: task.id, status: task.status, due: task.due})
-      } //marks corresponding task complete, turning its text green
+          🗑️
+        </button>
+        <button className='markComplete' onClick={() => {
+          confetti()
+          dispatch({ type: 'markComplete', name: taskName, id: task.id, status: task.status, due: task.due })
+        } //marks corresponding task complete, turning its text green
         }>
-      Complete
-      </button>
+          ✅
+        </button>
       </div>
-      )
+    )
   })
 
   return (
     <div className='addDiv'>
       <h1>To Do List</h1>
-      <div className='displayDiv'>
-        { display }
-      </div>
-      <div>
-        <input id = 'addField'
-          value = { taskName }
-          onChange={(e) => {
-            setTaskName(e.target.value) //sets input value to be added as task name
-          }
-        }
-        > 
-        </input>
-        {/* current date is automatically displayed in input; clicking input brings up a calander to select a date
-        whatever date is in the input when 'add' is clicked will be set as task due date */}
-        <DatePicker selected={dueDate} onChange={(dueDate) => setDueDate(dueDate)} /> 
-        <button className = 'addTask' onClick = {() => {
-          if (taskName !== '') { //prevents empty task from being added
-            dispatch({ type:'addTask', name: taskName, id: uuidv4(), status: 'incomplete', due: dueDate}) //adds task to be displayed
-            console.log('due = ', dueDate)
-            console.log('number = ', dueDate.getTime())
-          } 
-            setTaskName('') //clears input field
+      <div id='displayDiv'>
+        {display}
+        <div>
+          <input id='addField'
+            value={taskName}
+            onChange={(e) => {
+              setTaskName(e.target.value) //sets input value to be added as task name
             }
+            }
+          >
+          </input>
+          {/* current date is automatically displayed in input; clicking input brings up a calander to select a date
+        whatever date is in the input when 'add' is clicked will be set as task due date */}
+          <DatePicker id='datePicker' selected={dueDate} onChange={(dueDate) => setDueDate(dueDate)} />
+          <button className='addTask' onClick={() => {
+            if (taskName !== '') { //prevents empty task from being added
+              dispatch({ type: 'addTask', name: taskName, id: uuidv4(), status: 'incomplete', due: dueDate }) //adds task to be displayed
+              console.log('due = ', dueDate)
+              console.log('number = ', dueDate.getTime())
+            }
+            setTaskName('') //clears input field
           }
-        >
-          Add
-        </button>
+          }
+          >
+            Add
+          </button>
+        </div>
       </div>
     </div>
   )
